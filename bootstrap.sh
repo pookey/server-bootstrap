71762e679fe55afdee9b8ac416ec715a2e44f690
Ian P. Christian <pookey@gmail.com>
2019-04-13 07:06:59 +0000

    fixing librarian instal

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ apt-get install puppet librarian-puppet -y
 cd /tmp
 git clone https://github.com/pookey/server-bootstrap.git
 cd /tmp/server-bootstrap/puppet/
-librarian-puppet init
+librarian-puppet install
 puppet apply ./manifests/site.pp  --modulepath /tmp/server-bootstrap/puppet/modules/
 
 
